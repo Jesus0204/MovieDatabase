ae488def207fcc4b001df1ae3a34a9e26c07ed53
Jesus0204 <65917649+Jesus0204@users.noreply.github.com>
2023-09-13 14:18:15 -0600

    Sort con num y text

--- pruebas.cpp
+++ pruebas.cpp
@@ -8,7 +8,7 @@ int main(){
     // Creas la base de datos vacía con el arreglo movies[]
     Movie_Database Yeesus_movies;
 
-    // Agrega las 3 películas al arreglo
+    // Agrega las 5 películas al arreglo
     Yeesus_movies.agrega_movies();
 
     cout << "Antes de ordenar" << endl;
@@ -16,12 +16,21 @@ int main(){
     // Imprime películas antes de ordenar
     Yeesus_movies.print_movies();
 
-    // Llama el selection sort para ordenarlo con review
+    // Llama el selection sort para ordenarlo con year
     Yeesus_movies.sort_num(5);
 
-    cout << "Despues de ordenar;" << endl;
+    cout << "Despues de ordenar:" << endl;
 
     // Imprime el arreglo despues de ordenar
+    Yeesus_movies.print_movies();
+
+    // Ahora se hace lo mismo pero con texto (como nombre o directores)
+
+    // Llama el selection sort para ordenarlo por nombre
+    Yeesus_movies.sort_text(1);
+
+    cout << "Despues de ordenar por nombre:" << endl;
+
     Yeesus_movies.print_movies();
 
 }
